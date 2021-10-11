07b0b1f56b2f7216c3b44508dd6de02fe83909a3
Miljana Botunjac <miljanabotunjac@gmail.com>
2021-10-11 14:17:09 +0200

    toggle style fixed

--- script.js
+++ script.js
@@ -3,8 +3,44 @@ const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 const calculator = document.querySelector(`.calculator`)
 const keys = calculator.querySelector(`.keys`)
 
+
+const key = e.target
+const action = key.dataset.action
+
+//key listener
 keys.addEventListener(`clik`, e => {
  if (e.target.matches(`button`)) {
    // Do something
  }
 })
+
+if (!action) {
+    console.log('number key!')
+  }
+
+
+  /*
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    console.log('operator key!')
+  }
+
+
+
+  if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+  
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+  
+  if (action === 'calculate') {
+    console.log('equal key!')
+  }
+*/
+  
